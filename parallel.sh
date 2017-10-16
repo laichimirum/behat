de8bd2a3fd4a088f953ee28381d280cdcd1ab96e
laichimirum <lajos.hanko@mirumagency.com>
2017-10-16 09:55:25 +0200

    Run tests in parallel

--- parallel.sh
+++ parallel.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 IFS="," read -ra PARAMS <<< "$1"
+DIRDATE=`date '+%Y-%m-%d'`
+
+if [ ! -d "$DIRDATE" ]; then
+    mkdir results/$DIRDATE
+fi
 
 for i in ${PARAMS[@]}; do
-    d=`date '+%Y-%m-%d-%H%M%S'`
-    bin/behat -p $i  --format-settings='{"paths": false}' >> "results/$i-$d.txt" &
+    RUNDATE=`date '+%Y-%m-%d-%H%M%S'`
+    bin/behat -p $i  --format-settings='{"paths": false}' >> "results/$DIRDATE/$i-$RUNDATE.txt" &
     echo "Running tests in $i"
 done
 
